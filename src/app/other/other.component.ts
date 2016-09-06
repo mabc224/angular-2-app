0cd7bbb04c2a78dc690d923c6e603c03c9be57dd
mabc224 <mabc224@gmail.com>
2016-09-06 08:15:43 +0500

    added new compenet with inline style and add in other component

---
+++ src/app/other/other.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-other-compenent',
+  templateUrl: 'other.component.html',
+  styles: [`
+    h1 {
+      color: red;
+    }
+  `]
+})
+export class OtherCompenent {
+
+}
